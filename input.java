ef4c6845989a89db7c69e4ed48c29f5607bf4207
PatanMahaboobSubhani <psubbusubhani@gmail.com>
2024-10-29 18:16:18 +0000

    new commit added

---
+++ input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class main{
+public static void main(String [] args){
+Scanner scan = new Scanner (System.in);
+System.out.print("Enter your number: +91");
+int number = scan.nextInt();
+System.out.println("Your number will be added successfully into our database");
+}
+}
